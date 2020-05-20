0e0d98e08a92e3eaa985034be3e7e33faee70345
Plinio <plinio.duartes@hotmail.com>
2020-05-19 22:06:53 -0300

    status

--- src/pages/formulario/CREATE_LINE.js
+++ src/pages/formulario/CREATE_LINE.js
@@ -4,6 +4,8 @@
     31 é o índice do CNPJ da empresa,
     32 é o DACTE, A partir do 27º digito e contém 8 characteres
 */
+import { statuses } from './statuses';
+
 export const create_line = async (item) => { 
     var nfs = [];
     var string = '';
@@ -18,11 +20,13 @@ export const create_line = async (item) => {
     // console.log(item.NOTAFISCAL, item.DATA_EVENTO, item.CNPJ, item.DACTE)
 
     return new Promise( async (resolve) => {
-        if (item.STATUS === "ENTREGUE") {  // Cria a linha só se o status for igual a ENTREGUE
+        var item_status = statuses()[item.STATUS];
+
+        if (item.STATUS !== null && item.STATUS !== undefined && item.DACTE !== undefined && item_status !== null && item_status !== undefined) {
             cnpj = item.CNPJ;
             nfs = await item.NOTAFISCAL.includes('/') ? 
-            item.NOTAFISCAL.split('/') :
-            [item.NOTAFISCAL];
+                item.NOTAFISCAL.split('/') :
+                [item.NOTAFISCAL];
             
             let date_not_formatted = (item.DATA_EVENTO.split(' '))[0];  // Formatando a data
             let date_fields = date_not_formatted.split('/');    // Formatando a data
@@ -31,7 +35,7 @@ export const create_line = async (item) => {
             let hours_not_formatted = (item.DATA_EVENTO.split(' '))[1]; // Formatando a horas
             let hours_field = hours_not_formatted.split(':');   // Formatando a horas
             hours = `${ hours_field[0] }${ hours_field[1] }`;   // Formatando a horas
-
+            
             DACTE = item.DACTE.substring(26, 34);
 
             nfs.forEach( async (nf, index) => {    // Montar cada linha aqui dentro
@@ -40,7 +44,7 @@ export const create_line = async (item) => {
 
                 new Promise( async (resolve) => {
 
-                    string = string + `${ initial_code }${ cnpj }${ digit_nf }  0000${ nf_without_digit }001${ date }${ hours }00                                                                                00000000010    ${ DACTE }                                                                                                     \n`;
+                    string = string + `${ initial_code }${ cnpj }${ digit_nf }  0000${ nf_without_digit }${ statuses()[item.STATUS] }${ date }${ hours }00                                                                                00000000010    ${ DACTE }                                                                                                     \n`;
                     resolve(string);
 
                 }).then( async (res) => {
