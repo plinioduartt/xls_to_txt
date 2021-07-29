4a023dd46cba7f505281a5322942c529d5a04f20
Plinio <plinio.duartes@hotmail.com>
2021-07-29 11:25:59 -0300

    fix: new version

--- src/pages/formulario/CREATE_LINE.js
+++ src/pages/formulario/CREATE_LINE.js
@@ -4,9 +4,9 @@
     31 é o índice do CNPJ da empresa,
     32 é o DACTE, A partir do 27º digito e contém 8 characteres
 */
-import { statuses } from './statuses';
+import { statuses } from './updated_statuses';
 
-export const create_line = async (item) => { 
+export const create_line = async (item) => {
     var nfs = [];
     var string = '';
     var initial_code = '542';
@@ -19,45 +19,65 @@ export const create_line = async (item) => {
 
     // console.log(item.NOTAFISCAL, item.DATA_EVENTO, item.CNPJ, item.DACTE)
 
-    return new Promise( async (resolve) => {
-        var item_status = statuses()[item.STATUS];
+    return new Promise(async (resolve, reject) => {
+        var item_status = statuses(item.STATUS.toUpperCase());
 
-        if (item.STATUS !== null && item.STATUS !== undefined && item.DACTE !== undefined && item_status !== null && item_status !== undefined) {
+        console.log('item_status', item_status)
+
+        if (item.STATUS && item.DACTE && item_status) {
             cnpj = item.CNPJ;
-            nfs = await item.NOTAFISCAL.includes('/') ? 
+            nfs = await item.NOTAFISCAL.includes('/') ?
                 item.NOTAFISCAL.split('/') :
                 [item.NOTAFISCAL];
-            
+
             let date_not_formatted = (item.DATA_EVENTO.split(' '))[0];  // Formatando a data
             let date_fields = date_not_formatted.split('/');    // Formatando a data
-            date = `${ date_fields[0] }${ date_fields[1] }${ date_fields[2] }`; // Formatando a data
+            date = `${date_fields[0]}${date_fields[1]}${date_fields[2]}`; // Formatando a data
 
             let hours_not_formatted = (item.DATA_EVENTO.split(' '))[1]; // Formatando a horas
             let hours_field = hours_not_formatted.split(':');   // Formatando a horas
-            hours = `${ hours_field[0] }${ hours_field[1] }`;   // Formatando a horas
-            
+            hours = `${hours_field[0]}${hours_field[1]}`;   // Formatando a horas
+
             DACTE = item.DACTE.substring(26, 34);
 
-            nfs.forEach( async (nf, index) => {    // Montar cada linha aqui dentro
+            nfs.map(async (nf, index) => {    // Montar cada linha aqui dentro
                 digit_nf = (nf.split('-'))[1];
                 nf_without_digit = (nf.split('-'))[0];
 
-                new Promise( async (resolve2) => {
+                var nf_number_without_digit = nf_without_digit.length > 0 ?
+                    `${new Array(10 - nf_without_digit.length).join('0')}${nf_without_digit}` :
+                    'NFe_Invalida';
+                const free_text_space = new Array(128 - 47).join(' ');
+                const last_string = new Array(251 - 150).join(' ')
 
-                    string = string + `${ initial_code }${ cnpj }${ digit_nf }  0000${ nf_without_digit }${ statuses()[item.STATUS] }${ date }${ hours }00                                                                                00000000010    ${ DACTE }                                                                                                     \n`;
-                    resolve2(string);
+                // string = string + `${initial_code}${cnpj}${digit_nf}  0000${nf_without_digit}${item_status}${date}${hours}00                                                                              00000000010    ${DACTE}                                                                                                     \n`;
 
-                }).then( async (res) => {
+                string = string + `${initial_code}${cnpj.replace('.', '').replace('.', '').replace('/', '').replace('-', '')}${digit_nf}  ${nf_number_without_digit}${item_status}${date}${hours}00${free_text_space}00000000010    ${DACTE}${last_string}\n`
 
-                    if (index === (nfs.length - 1)) {
-                        // console.log(index, (nfs.length - 1), res)
-                        await resolve(res);
-                    }
-
-                });                
-                
+                if (index === (nfs.length - 1)) {
+                    // console.log(index, (nfs.length - 1), res)
+                    resolve(string);
+                }
             });
+        } else {
+            const getErrorMessage = () => {
+                if (!item.DACTE) {
+                    return `Oops! Ocorreu um erro no CTE "${item.CTE}". O DACTE deste documento é inválido.`
+                }
+
+                if (!item.STATUS) {
+                    return `Oops! Ocorreu um erro no CTE "${item.CTE}". O status deste documento é inválido.`
+                }
+
+                if (!item_status) {
+                    return `Oops! Ocorreu um erro no CTE "${item.CTE}". O status "${item.STATUS}" não foi encontrado no banco de códigos.`
+                }
+            };
+
+            reject({
+                message: getErrorMessage()
+            })
         }
     });
-}   
+}
 
--- src/pages/formulario/formulario.js
+++ src/pages/formulario/formulario.js
@@ -3,121 +3,137 @@ import * as XLSX from 'xlsx';
 import Loader from '../../assets/loader.gif';
 import McLovin from '../../assets/mclovin.jpg';
 
+import { FaUpload } from 'react-icons/fa';
+
 import './formulario.css';
-import upload_image from './../../assets/upload.png';
 import { create_line } from './CREATE_LINE';
 
-var string =             '000JADLOG LOGISTICA S.A               ELSYS EQUIPAMENTOS ELETRONICOS LTDA0101190000OCO502000000                                                                                                                                                           \n';
-var second_line_string = '540OCORR502000000                                                                                                                                                                                                                                         \n';
-var thirdy_line_string = '54104884082000135JADLOG LOGISTICA S.A                                                                                                                                                                                                                     \n'
+var header = '000JADLOG LOGISTICA S.A               ELSYS EQUIPAMENTOS ELETRONICOS LTDA0101190000OCO502000000                                                                                                                                                          \n';
+var second_line_header = '540OCORR502000000                                                                                                                                                                                                                                        \n';
+var thirdy_line_header = '54104884082000135JADLOG LOGISTICA S.A                                                                                                                                                                                                                    \n'
 
-string = `${ string }${ second_line_string }${ thirdy_line_string }`;
-
+var string = `${header}${second_line_header}${thirdy_line_header}`;
 
 export default class Formulario extends Component {
-    state = { 
-        selected: false, 
-        show_button: false,
-        show_loader: false,
-        file: null
-    };
+  state = {
+    selected: false,
+    show_button: false,
+    show_loader: false,
+    file: null
+  };
 
-    async upload(e) {
-        if (e.target.files[0] !== undefined) {
-            this.setState({ show_loader: true });
-        
-            let name = e.target.files[0].name;
-    
-            if (!name.includes('.xls') && !name.includes('.xlsx')) {
-                this.setState({ show_loader: false });
-                return alert('Formato incorreto, Tente um arquivo XLS ou XLSX');
-            } else {
-                await this.setState({ file: e.target.files[0], selected: true, show_button: false });
-    
-                var dataParse;
-                var reader = new FileReader();
-                reader.onload = async (e) => {
-                    var data = e.target.result;
-                    let readedData = XLSX.read(data, {type: 'binary'});
-                    const wsname = readedData.SheetNames[0];
-                    const ws = readedData.Sheets[wsname];
+  async upload(e) {
+    if (e.target.files[0] !== undefined) {
+      this.setState({ show_loader: true });
+
+      let name = e.target.files[0].name;
+
+      if (!name.includes('.xls') && !name.includes('.xlsx')) {
+        this.setState({ show_loader: false });
+        return alert('Formato incorreto, Tente um arquivo XLS ou XLSX');
+      } else {
+        await this.setState({ file: e.target.files[0], selected: true, show_button: false });
+
+        var dataParse;
+        var reader = new FileReader();
+        reader.onload = async (e) => {
+          var data = e.target.result;
+          let readedData = XLSX.read(data, { type: 'binary' });
+          const wsname = readedData.SheetNames[0];
+          const ws = readedData.Sheets[wsname];
+
+          /* Convert array to json*/
+          dataParse = XLSX.utils.sheet_to_json(ws, { header: 0 });
+          // delete dataParse[0]; // Removendo os nomes das colunas
+          // delete dataParse[dataParse.length-1];
+          // delete dataParse[dataParse.length-2];
+
+          console.log("Data ==> ", dataParse)
+
+          if (!dataParse[0].DACTE) {
+            this.setState({ show_loader: false, file: null, selected: false });
+            return alert('Formato incorreto, tente novamente.');
+          }
+
+          var timeout;
+          dataParse.map(async (item, index) => {
+            clearTimeout(timeout);
             
-                    /* Convert array to json*/
-                    dataParse = XLSX.utils.sheet_to_json(ws, {header:0});
-                    // delete dataParse[0]; // Removendo os nomes das colunas
-                    // delete dataParse[dataParse.length-1];
-                    // delete dataParse[dataParse.length-2];
-                   
-                    var timeout;
-                    await dataParse.forEach( async (item, index) => {
-                        clearTimeout(timeout);
-
-                        let line = await create_line(item)
-                        string = `${ string }${ line }`;
-                    
-                        timeout = setTimeout( async () => {
-                            // if (index === (dataParse.length - 3)) {
-                                // console.log('STRING', string);
-
-                                await this.create_txt_file(string);
-                                setTimeout( async () => {
-                                    this.setState({ show_loader: false, show_button: true });
-                                }, 3000);
-                            // }
-                        }, 2000);                       
-                    });
-        
-                };
-                reader.readAsBinaryString(this.state.file)
-            }        
-        }        
-    }
-
-    async create_txt_file(text) {
-        var textFile = null,
-        makeTextFile = function (text) {
-            var data = new Blob([text], {type: 'text/plain'});
-        
-            if (textFile !== null) {
-                window.URL.revokeObjectURL(textFile);
+            try {
+              let line = await create_line(item)
+              string = `${string}${line}`;
+            } catch (error) {
+              this.setState({ show_loader: false, file: null, selected: false });
+              return alert(error.message);
             }
-            
-            textFile = window.URL.createObjectURL(data);
-            
-            return textFile;
+
+            timeout = setTimeout(async () => {
+              // if (index === (dataParse.length - 3)) {
+              // console.log('STRING', string);
+
+              await this.create_txt_file(string);
+
+              setTimeout(async () => {
+                this.setState({ show_loader: false, show_button: true });
+              }, 3000);
+              // }
+            }, 2000);
+          });
+
         };
-    
-        setTimeout(() => {
-            var link = document.getElementById('downloadlink');
-            text = text + '549                                                                                                                                                                                                                                                       \n';
-            link.href = makeTextFile(text);
-            link.style.display = 'block';
-            string = '000JADLOG LOGISTICA S.A               ELSYS EQUIPAMENTOS ELETRONICOS LTDA0101190000OCO502000000                                                                                                                                                           \n';
-            string = `${ string }${ second_line_string }${ thirdy_line_string }`;
-        }, 3000);
+        reader.readAsBinaryString(this.state.file)
+      }
     }
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <p id="desenvolvido-por">Desenvolvido por</p>
-                <div id="avatar">
-                    {/* <img alt="" src={ McLovin } /> */}
-                    <p>@MK3Digital</p>
-                </div>
+  async create_txt_file(text) {
+    try {
+      var textFile = null,
+        makeTextFile = function (text) {
+          var data = new Blob([text], { type: 'text/plain' });
 
-                <div className="App-header">
-                    <p className="App-link">Upload da planilha (XLSX)</p>
-                    <label for="file-upload" className="custom-file-upload">
-                        <img alt="" src={ upload_image } />
-                        { this.state.selected ? this.state.file.name : 'Nenhum arquivo selecionado' }
-                    </label>
-                    <input id="file-upload" className="upload-file" onChange={ (e) => this.upload(e) } type="file"/>
-                    <img className={ this.state.show_loader ? 'active' : 'disabled' } id="loader" alt="" src={ Loader } />
-                    <a href="" id="downloadlink" download={ this.state.selected ? `${ (this.state.file.name.split('.'))[0] }.txt`: 'test.txt' } className={ this.state.show_button ? 'active' : 'disabled'}>Download</a>
-                </div>
-            </div>
-        );
+          if (textFile !== null) {
+            window.URL.revokeObjectURL(textFile);
+          }
+
+          textFile = window.URL.createObjectURL(data);
+
+          return textFile;
+        };
+
+      setTimeout(() => {
+        var link = document.getElementById('downloadlink');
+        text = text + '549                                                                                                                                                                                                                                                      \n';
+        link.href = makeTextFile(text);
+        link.style.display = 'block';
+        string = `${header}${second_line_header}${thirdy_line_header}`;
+      }, 3000);
+    } catch (error) {
+      console.log('ERROR ====> ', error);
     }
-    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/*<p id="desenvolvido-por">Desenvolvido por</p>
+        <div id="avatar">
+           <img alt="" src={ McLovin } /> 
+          <p>@MK3Digital</p>
+        </div>*/}
+
+        <div className="App-header">
+          <p className="App-link">Upload da planilha (XLS / XLSX)</p>
+          <label for="file-upload" className="custom-file-upload">
+            <FaUpload className="upload-icon" />
+            {this.state.selected ? this.state.file.name : 'Nenhum arquivo selecionado'}
+          </label>
+          <input id="file-upload" className="upload-file" onChange={(e) => this.upload(e)} type="file" />
+          <img className={this.state.show_loader ? 'active' : 'disabled'} id="loader" alt="" src={Loader} />
+          <a href="" id="downloadlink" download={this.state.selected ? `${(this.state.file.name.split('.'))[0]}.txt` : 'test.txt'} className={this.state.show_button ? 'active' : 'disabled'}>Download</a>
+        </div>
+      </div>
+    );
+  }
+
 }
